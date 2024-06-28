7a08dc915e546affdd3679d8011253819c8c29ba
Demonstrandum <samuel@knutsen.co>
2024-06-28 19:55:02 +0100

    bug: html void tags would stop any further generation.
    Due to using `return Ok(())` instead of a `continue` after finishing
    generation early for html void elements, the html generation would just
    stop after the first such tag.  This was fixed with a refactor, taking
    the individual node generation out into its own function, and converting
    all the old `continue`s into `return`s.

--- src/assemble/html.rs
+++ src/assemble/html.rs
@@ -98,189 +98,193 @@ impl<'a> MarkupDisplay for HTMLFormatter<'a> {
         Ok(doc)
     }
 
-    fn generate(&self, f : Formatter)
-    -> Result<(), GenerationError> {
+    fn generate(&self, f: Formatter) -> Result<(), GenerationError> {
         let mut tree_iter = self.tree.iter().peekable();
         while let Some(node) = tree_iter.next() {
-            match node {
-                ParseNode::Symbol(node)
-                | ParseNode::Number(node) => {
-                    write!(f, "{}", node.leading_whitespace)?;
-                    write!(f, "{}", escape_xml(&node.value))?;
-                },
-                ParseNode::String(node) => {
-                    write!(f, "{}", node.leading_whitespace)?;
-                    write!(f, "{}", escape_xml(&node.value))?;
-                },
-                ParseNode::List { nodes: list, leading_whitespace, end_token, .. } => {
-                    write!(f, "{}", leading_whitespace)?;
-                    let head = list.first();
-                    let tag: &str;  // html <tag> name.
-                    if let Some(head_node) = head {
-                        if let ParseNode::Symbol(head_symbol) = head_node {
-                            tag = &head_symbol.value;
-                            write!(f, "<{}", tag)?;
-                        } else {
-                            // Error, tags can only have symbol values.
-                            return Err(GenerationError::new("HTML",
-                                "HTML tags can only be given as symbols.",
-                                head_node.site()));
-                        }
-                    } else {
-                        // Error, empty tags not supported.
-                        return Err(GenerationError::new("HTML",
-                            "Empty lists cannot be converted into a valid HTML tag.",
-                            node.site()));
-                    }
-                    let tag = tag.to_ascii_lowercase();
-
-                    let mut rest = &list[1..];
-
-                    // Declarations behave differently.
-                    if tag.as_bytes()[0] == '!' as u8 {
-                        while !rest.is_empty() {
-                            if let Some(node) = rest[0].symbolic() {
-                                write!(f, " {}", node.value)?;
-                            } else {
-                                return Err(GenerationError::new("HTML",
-                                    "Non-symbolic item in declaration",
-                                    &rest[0].site()));
-                            }
-                            rest = &rest[1..];
-                        }
-                        write!(f, ">")?;
-                        continue;
-                    }
-
-                    while let Some(ParseNode::Attribute { node, keyword, .. }) = rest.first() {
-                        if let Some(atom) = (*node).atomic() {
-                            write!(f, " {}=\"{}\"", keyword, atom.value)?;
-                            rest = &rest[1..];
-                        } else {
-                            // Error! Cannot be non atomic.
-                            return Err(GenerationError::new("HTML",
-                                "Attribute cannot contain non-atomic data.",
-                                &(*node).site()));
-                        }
-                    }
-                    write!(f, ">")?;
-
-                    // Check early if this tag is a void element.
-                    if VOID_ELEMENTS.binary_search(&tag.as_str()).is_ok() {
-                        // Void elements cannot have children.
-                        if let Some(child_node) = rest.first() {
-                            return Err(GenerationError::new("HTML",
-                                &format!("A void element such as `<{}>' cannot have children.", tag),
-                                child_node.site()));
-                        }
-                        // Finished: void elements dont get a closing tag.
-                        return Ok(());
-                    }
-
-                    // The first node to a tag should have its whitespace supressed!
-                    // e.g. `(p hello world)` -> `<p>hello world</p>`.
-                    // But if there's a new line, its likely it should be carreid through.
-                    // e.g.
-                    // ```
-                    // (div
-                    //    hello)
-                    // ```
-                    // ->
-                    // ```
-                    // <div>
-                    //    hello
-                    // </div>
-                    let rest_with_preserved_whitespace = rest;
-                    let mut rest: Vec<ParseNode<'a>> = rest_with_preserved_whitespace.to_vec();
-                    let mut is_first_node_on_next_line = false;
-                    if let Some(first_node) = rest.get_mut(0) {
-                        is_first_node_on_next_line = first_node.leading_whitespace().contains('\n');
-                        if !is_first_node_on_next_line {
-                            first_node.set_leading_whitespace("".to_owned());
-                        }
-                    }
-
-                    // Handle tags which *do not* contain HTML as syntax:
-                    //    <pre>, <style>, <script>, <math>, <svg>, <textarea>, <title>
-                    // Specifically:
-                    //   - <svg> and <math> contain XML, not HTML;
-                    //   - <pre>, <textarea> and <title> contain raw text, not parsed as HTML;
-                    //   - <pre> will display raw text found in source code;
-                    //   - <textarea> and <title> however, are escapable (evaluete macros);
-                    //   - <script> contains JavaScript, maybe we will parse this in the future!;
-                    //   - <style> contains CSS, which we have our own parser for already.
-                    match tag.as_str() {
-                        "pre" => { // <pre> should preserve the raw text in the *source* file.
-                            // Find beginning and end byte offset of first and last token inside
-                            // of `(pre ...)` and simply clone the text between those offsets.
-                            let pre = raw_text(rest_with_preserved_whitespace.first(), end_token);
-                            write!(f, "{}", pre)?;
-                        },
-                        "textarea" | "title" => { // Not eaw source-code, but plain-text.
-                            // We have to reconsititute what the source-code would look like if all
-                            // macros were expanded by hand, and read as raw source code.
-                            let sexp_fmt = SExpFormatter::new(rest.into_boxed_slice());
-                            let sexp_fmt = Box::leak(Box::new(sexp_fmt)); // TODO: Store.
-                            sexp_fmt.generate(f)?;
-                        },
-                        "style" => {  // <style> tag needs to generate CSS.
-                            // When just a string is passed, don't convert. Assume raw CSS.
-                            if let Some(ParseNode::String(string_node)) = rest.first() {
-                                if rest.len() != 1 {
-                                    // FIXME: Leak doesn't really matter, but should really be a better way.
-                                    let second_node = Box::leak(Box::new(rest[1].to_owned()));
-                                    return Err(GenerationError::new("HTML+CSS",
-                                        "A `style' tag can either have S-expression CSS rules, or\
-                                         a single string containing raw CSS be passed in.\n\
-                                         A string was passed in, but excess expressions were passed \
-                                         in after that!",
-                                        second_node.site()));
-                                }
-                                // Otherwise, write that raw CSS.
-                                write!(f, "{}", string_node.value)?;
-                            } else {
-                            writeln!(f, "")?;
-                                let css_fmt = CSSFormatter::new(rest.into_boxed_slice());
-                                let css_fmt = Box::leak(Box::new(css_fmt)); // FIXME: store formatter.
-                                css_fmt.generate(f)?;
-                            }
-                        },
-                        "script" => {
-                            // TODO: Generating JavaScript from S-expressions is not implemented.
-                            // For now, just treat it as a raw-text tag (a la <pre>).
-                            let sexp_fmt = SExpFormatter::new(rest.into_boxed_slice());
-                            let sexp_fmt = Box::leak(Box::new(sexp_fmt)); // TODO: Store.
-                            sexp_fmt.generate(f)?;
-                        },
-                        "math" | "svg" => {  // <math> and <svg> are subsets of XML.
-                            let xml_fmt = XMLFormatter::new(rest.into_boxed_slice());
-                            let xml_fmt = Box::leak(Box::new(xml_fmt)); // FIXME: store formatter.
-                            xml_fmt.generate(f)?;
-                        },
-                        _ => {  // Tag contains regular old HTML.
-                            let html_fmt = HTMLFormatter::new(rest.into_boxed_slice());
-                            let html_fmt = Box::leak(Box::new(html_fmt)); // FIXME: store formatter.
-                            html_fmt.generate(f)?;
-                        },
-                    }
-                    // Closing tag should be equally as spaced as opening tag (?)
-                    if end_token.leading_whitespace.is_empty() {
-                        if is_first_node_on_next_line || tag == "style" {
-                            write!(f, "{}", leading_whitespace)?;
-                        }
-                    } else {
-                        write!(f, "{}", end_token.leading_whitespace)?;
-                    }
-
-                    write!(f, "</{}>", tag)?;
-                },
-                ParseNode::Attribute { ref site, .. } =>
-                    return Err(GenerationError::new("HTML",
-                        "Unexpected attribute encountered.", site))
-            }
+            generate_html_node(f, node)?;
         }
         Ok(())
     }
+}
+
+fn generate_html_node<'a>(f: Formatter, node: &'a ParseNode<'a>) -> Result<(), GenerationError<'a>> {
+    match node {
+        ParseNode::Symbol(node)
+        | ParseNode::Number(node) => {
+            write!(f, "{}", node.leading_whitespace)?;
+            write!(f, "{}", escape_xml(&node.value))?;
+        },
+        ParseNode::String(node) => {
+            write!(f, "{}", node.leading_whitespace)?;
+            write!(f, "{}", escape_xml(&node.value))?;
+        },
+        ParseNode::List { nodes: list, leading_whitespace, end_token, .. } => {
+            write!(f, "{}", leading_whitespace)?;
+            let head = list.first();
+            let tag: &str;  // html <tag> name.
+            if let Some(head_node) = head {
+                if let ParseNode::Symbol(head_symbol) = head_node {
+                    tag = &head_symbol.value;
+                    write!(f, "<{}", tag)?;
+                } else {
+                    // Error, tags can only have symbol values.
+                    return Err(GenerationError::new("HTML",
+                        "HTML tags can only be given as symbols.",
+                        head_node.site()));
+                }
+            } else {
+                // Error, empty tags not supported.
+                return Err(GenerationError::new("HTML",
+                    "Empty lists cannot be converted into a valid HTML tag.",
+                    node.site()));
+            }
+            let tag = tag.to_ascii_lowercase();
+
+            let mut rest = &list[1..];
+
+            // Declarations behave differently.
+            if tag.as_bytes()[0] == '!' as u8 {
+                while !rest.is_empty() {
+                    if let Some(node) = rest[0].symbolic() {
+                        write!(f, " {}", node.value)?;
+                    } else {
+                        return Err(GenerationError::new("HTML",
+                            "Non-symbolic item in declaration",
+                            &rest[0].site()));
+                    }
+                    rest = &rest[1..];
+                }
+                write!(f, ">")?;
+                return Ok(());
+            }
+
+            while let Some(ParseNode::Attribute { node, keyword, .. }) = rest.first() {
+                if let Some(atom) = (*node).atomic() {
+                    write!(f, " {}=\"{}\"", keyword, atom.value)?;
+                    rest = &rest[1..];
+                } else {
+                    // Error! Cannot be non atomic.
+                    return Err(GenerationError::new("HTML",
+                        "Attribute cannot contain non-atomic data.",
+                        &(*node).site()));
+                }
+            }
+            write!(f, ">")?;
+
+            // Check early if this tag is a void element.
+            if VOID_ELEMENTS.binary_search(&tag.as_str()).is_ok() {
+                // Void elements cannot have children.
+                if let Some(child_node) = rest.first() {
+                    return Err(GenerationError::new("HTML",
+                        &format!("A void element such as `<{}>' cannot have children.", tag),
+                        child_node.site()));
+                }
+                // Finished: void elements don't get a closing tag.
+                return Ok(());
+            }
+
+            // The first node to a tag should have its whitespace supressed!
+            // e.g. `(p hello world)` -> `<p>hello world</p>`.
+            // But if there's a new line, its likely it should be carreid through.
+            // e.g.
+            // ```
+            // (div
+            //    hello)
+            // ```
+            // ->
+            // ```
+            // <div>
+            //    hello
+            // </div>
+            let rest_with_preserved_whitespace = rest;
+            let mut rest: Vec<ParseNode<'a>> = rest_with_preserved_whitespace.to_vec();
+            let mut is_first_node_on_next_line = false;
+            if let Some(first_node) = rest.get_mut(0) {
+                is_first_node_on_next_line = first_node.leading_whitespace().contains('\n');
+                if !is_first_node_on_next_line {
+                    first_node.set_leading_whitespace("".to_owned());
+                }
+            }
+
+            // Handle tags which *do not* contain HTML as syntax:
+            //    <pre>, <style>, <script>, <math>, <svg>, <textarea>, <title>
+            // Specifically:
+            //   - <svg> and <math> contain XML, not HTML;
+            //   - <pre>, <textarea> and <title> contain raw text, not parsed as HTML;
+            //   - <pre> will display raw text found in source code;
+            //   - <textarea> and <title> however, are escapable (evaluete macros);
+            //   - <script> contains JavaScript, maybe we will parse this in the future!;
+            //   - <style> contains CSS, which we have our own parser for already.
+            match tag.as_str() {
+                "pre" => { // <pre> should preserve the raw text in the *source* file.
+                    // Find beginning and end byte offset of first and last token inside
+                    // of `(pre ...)` and simply clone the text between those offsets.
+                    let pre = raw_text(rest_with_preserved_whitespace.first(), end_token);
+                    write!(f, "{}", pre)?;
+                },
+                "textarea" | "title" => { // Not eaw source-code, but plain-text.
+                    // We have to reconsititute what the source-code would look like if all
+                    // macros were expanded by hand, and read as raw source code.
+                    let sexp_fmt = SExpFormatter::new(rest.into_boxed_slice());
+                    let sexp_fmt = Box::leak(Box::new(sexp_fmt)); // TODO: Store.
+                    sexp_fmt.generate(f)?;
+                },
+                "style" => {  // <style> tag needs to generate CSS.
+                    // When just a string is passed, don't convert. Assume raw CSS.
+                    if let Some(ParseNode::String(string_node)) = rest.first() {
+                        if rest.len() != 1 {
+                            // FIXME: Leak doesn't really matter, but should really be a better way.
+                            let second_node = Box::leak(Box::new(rest[1].to_owned()));
+                            return Err(GenerationError::new("HTML+CSS",
+                                "A `style' tag can either have S-expression CSS rules, or\
+                                    a single string containing raw CSS be passed in.\n\
+                                    A string was passed in, but excess expressions were passed \
+                                    in after that!",
+                                second_node.site()));
+                        }
+                        // Otherwise, write that raw CSS.
+                        write!(f, "{}", string_node.value)?;
+                    } else {
+                    writeln!(f, "")?;
+                        let css_fmt = CSSFormatter::new(rest.into_boxed_slice());
+                        let css_fmt = Box::leak(Box::new(css_fmt)); // FIXME: store formatter.
+                        css_fmt.generate(f)?;
+                    }
+                },
+                "script" => {
+                    // TODO: Generating JavaScript from S-expressions is not implemented.
+                    // For now, just treat it as a raw-text tag (a la <pre>).
+                    let sexp_fmt = SExpFormatter::new(rest.into_boxed_slice());
+                    let sexp_fmt = Box::leak(Box::new(sexp_fmt)); // TODO: Store.
+                    sexp_fmt.generate(f)?;
+                },
+                "math" | "svg" => {  // <math> and <svg> are subsets of XML.
+                    let xml_fmt = XMLFormatter::new(rest.into_boxed_slice());
+                    let xml_fmt = Box::leak(Box::new(xml_fmt)); // FIXME: store formatter.
+                    xml_fmt.generate(f)?;
+                },
+                _ => {  // Tag contains regular old HTML.
+                    let html_fmt = HTMLFormatter::new(rest.into_boxed_slice());
+                    let html_fmt = Box::leak(Box::new(html_fmt)); // FIXME: store formatter.
+                    html_fmt.generate(f)?;
+                },
+            }
+            // Closing tag should be equally as spaced as opening tag (?)
+            if end_token.leading_whitespace.is_empty() {
+                if is_first_node_on_next_line || tag == "style" {
+                    write!(f, "{}", leading_whitespace)?;
+                }
+            } else {
+                write!(f, "{}", end_token.leading_whitespace)?;
+            }
+
+            write!(f, "</{}>", tag)?;
+        },
+        ParseNode::Attribute { ref site, .. } =>
+            return Err(GenerationError::new("HTML",
+                "Unexpected attribute encountered.", site))
+    }
+    Ok(())
 }
 
 /// Get raw text in source-file between a `start_node` and some `end_token`.
